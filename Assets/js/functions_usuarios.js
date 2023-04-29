3c760611e70849ab50c2517f17ef332d55ba8368
EsauRoncal <130332150+EsauRoncal@users.noreply.github.com>
2023-04-28 22:08:01 -0500

    Exportar Lista de usuario en excel pdf y cvs

--- Assets/js/functions_usuarios.js
+++ Assets/js/functions_usuarios.js
@@ -28,6 +28,34 @@ document.addEventListener('DOMContentLoaded', function(){
             {"data":"options"}
             
         ],
+        // Botones de exportaciones
+        'dom': 'lBfrtip',
+        'buttons': [
+            {
+                "extend":"copyHtml5",
+                "text":"<i class='far fa-copy'></i> Copiar",
+                "titleAttr":"Copiar",
+                "className": "btn btn-secondary"
+
+            },
+            {
+                "extend":"excelHtml5",
+                "text":"<i class='fas fa-file-excel'></i> Excel",
+                "titleAttr":"Exportar Excel",
+                "className": "btn btn-success"
+            },{
+                "extend":"pdfHtml5",
+                "text":"<i class='fas fa-file-pdf'></i> PDF",
+                "titleAttr":"Exportar PDF",
+                "className": "btn btn-danger"
+            },{
+                "extend":"csvHtml5",
+                "text":"<i class='fas fa-file-csv'></i> CSV",
+                "titleAttr":"Exportar CVS",
+                "className": "btn btn-info"
+
+            }
+        ],
 
         "resonsieve":"true",
         "bDestroy": true,
--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -24,6 +24,13 @@
     <script type="text/javascript" src="<?= media(); ?>/js/plugins/bootstrap-select.min.js"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
+    <!-- IMPORTACION DE LIBREIAS EN datatables.net -->
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+
     <!-- Condicionales Roles -->
     <?php if($data['page_name'] == "rol_usuario"){ ?>
         <script src="<?= media(); ?>/js/functions_roles.js"></script>
