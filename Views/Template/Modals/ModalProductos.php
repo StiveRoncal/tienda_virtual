b567e90cef52d2876e613ff38e9e76ce7f3c5a16
EsauRoncal <130332150+EsauRoncal@users.noreply.github.com>
2023-05-12 16:22:05 -0500

    Diseñar Formulario registro de productos

--- Views/Template/Modals/ModalProductos.php
+++ Views/Template/Modals/ModalProductos.php
@@ -1,6 +1,6 @@
 <!-- Modal Formulario Nuevo productos -->
 <div class="modal fade" id="modalFormProductos" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+  <div class="modal-dialog modal-xl">
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Producto</h5>
@@ -19,38 +19,71 @@
 
                 
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-8">
                         <div class="form-group">
-                          <label class="control-label">Nombre <span class="required">*</span></label>
-                          <input class="form-control" name="txtNombre" id="txtNombre" type="text" placeholder="Nombre Categoria" required="">
+                          <label class="control-label">Nombre Producto <span class="required">*</span></label>
+                          <input class="form-control" name="txtNombre" id="txtNombre" type="text" required="">
                         </div>
                         <div class="form-group">
-                          <label class="control-label">Descripción <span class="required">*</span></label>
-                          <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" rows="2" placeholder="Descripción La Categoria" required=""></textarea>
+                          <label class="control-label">Descripción Producto<span class="required">*</span></label>
+                          <textarea class="form-control" id="txtDescripcion" name="txtDescripcion" rows="4"></textarea>
                         </div>
                         
-                        <!-- ComboBox -->
-                        <div class="form-group">
-                            <label for="exampleSelect1">Estados <span class="required">*</span></label>
-                            <select class="form-control selectpicker" id="listStatus" name="listStatus" required="">
-                              <option value="1">Activo</option>
-                              <option value="2">Inactivo</option>
-                            </select>
-                        </div>    
+                        
 
 
 
                     </div>
 
-                    <div class="col-md-6">
-                        
-                 
+                    <div class="col-md-4">
+
+                            <div class="form-group">
+                                <label for="" class="control-label">Código <span class="required">*</span></label>
+                                <input class="form-control" id="txtCodigo" name="txtCodigo" type="text" placeholder="Código en Barra"  required="">
+                                <br>
+                            </div>
+                            
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label class="control-label">Precio <span class="required">*</span></label>
+                                    <input class="form-control " id="txtPrecio" name="txtPrecio" type="text" required="">
+                                </div>
+
+                                <div class="form-group col-md-6">
+                                    <label class="control-label">Stock <span class="required">*</span></label>
+                                    <input class="form-control" id="txtStock" name="txtStock" type="text"  required="">
+                                </div>
+                            </div>
 
 
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="listCategoria">Categoría <span class="required">*</span></label>
+                                    <select class="form-control" data-live-search="true" name="listCategoria" id="listCategoria" required=""></select>
+                                </div>
 
+                                <div class="form-group col-md-6">
+                                        <label for="listStatus">Estados <span class="required">*</span></label>
+                                        <select class="form-control selectpicker" id="listStatus" name="listStatus" required="">
+                                            <option value="1">Activo</option>
+                                            <option value="2">Inactivo</option>
+                                        </select>
+                                </div>
+                            </div>
 
+                      
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    
+                                    <button  id="btnActionForm" class="btn btn-primary btn-lg btn-block" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
 
+                                </div>
+                                <div class="form-group col-md-6">
+                                    
+                                    <button class="btn btn-danger btn-lg btn-block" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</button>
 
+                                </div>
+                            </div>    
 
 
                     </div>
@@ -60,8 +93,6 @@
 
                 <!-- Botones -->
                 <div class="tile-footer">
-                    <button  id="btnActionForm" class="btn btn-primary" type="submit"><i class="fa fa-fw fa-lg fa-check-circle"></i><span id="btnText">Guardar</span></button>&nbsp;&nbsp;&nbsp;
-                    <button class="btn btn-danger" type="button" data-dismiss="modal"><i class="fa fa-fw fa-lg fa-times-circle"></i>Cancelar</button>
                 </div>
 
                 
