3c434597ad34f2a6cc7c9ee64c1d6421e50e6906
EsauRoncal <130332150+EsauRoncal@users.noreply.github.com>
2023-05-22 12:48:23 -0500

    Configuracion de menu de navegacion

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -45,7 +45,7 @@
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Free shipping for standard order over $100
+						Bienvenido Usuario: Stive Roncal
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
@@ -54,15 +54,11 @@
 						</a>
 
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							My Account
+							Mi Cuenta
 						</a>
 
 						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							EN
-						</a>
-
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							USD
+							Salir / Iniciar Session
 						</a>
 					</div>
 				</div>
@@ -73,39 +69,29 @@
 					
 					<!-- Logo desktop -->		
 					<a href="#" class="logo">
-						<img src="<?= media();?>/tienda/images/icons/logo-01.png" alt="IMG-LOGO">
+						<!-- Implementar el logo GROSOTEC o NSRONCAL 133x17 -->
+						<img src="<?= media();?>/tienda/images/icons/logo-01.png" alt="Tienda Virtual">
 					</a>
 
 					<!-- Menu desktop -->
 					<div class="menu-desktop">
 						<ul class="main-menu">
 							<li class="active-menu">
-								<a href="index.html">Home</a>
-								<ul class="sub-menu">
-									<li><a href="index.html">Homepage 1</a></li>
-									<li><a href="home-02.html">Homepage 2</a></li>
-									<li><a href="home-03.html">Homepage 3</a></li>
-								</ul>
+								<a href="<?= base_url(); ?>">Inicio</a>
+								
 							</li>
 
 							<li>
-								<a href="product.html">Shop</a>
+								<a href="<?= base_url(); ?>/tienda">Tienda</a>
 							</li>
 
-							<li class="label1" data-label1="hot">
-								<a href="shoping-cart.html">Features</a>
+
+							<li>
+								<a href="<?= base_url(); ?>/nosotros">Nosotros</a>
 							</li>
 
 							<li>
-								<a href="blog.html">Blog</a>
-							</li>
-
-							<li>
-								<a href="about.html">About</a>
-							</li>
-
-							<li>
-								<a href="contact.html">Contact</a>
+								<a href="<?= base_url(); ?>/contacto">Contacto</a>
 							</li>
 						</ul>
 					</div>	
@@ -120,9 +106,7 @@
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 
-						<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
-							<i class="zmdi zmdi-favorite-outline"></i>
-						</a>
+						
 					</div>
 				</nav>
 			</div>	
@@ -132,7 +116,7 @@
 		<div class="wrap-header-mobile">
 			<!-- Logo moblie -->		
 			<div class="logo-mobile">
-				<a href="index.html"><img src="<?= media();?>/tienda/images/icons/logo-01.png" alt="IMG-LOGO"></a>
+				<a href="index.html"><img src="<?= media();?>/tienda/images/icons/logo-01.png" alt="Tienda Virtual"></a>
 			</div>
 
 			<!-- Icon header -->
@@ -145,9 +129,7 @@
 					<i class="zmdi zmdi-shopping-cart"></i>
 				</div>
 
-				<a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
-					<i class="zmdi zmdi-favorite-outline"></i>
-				</a>
+			
 			</div>
 
 			<!-- Button show menu -->
@@ -164,7 +146,7 @@
 			<ul class="topbar-mobile">
 				<li>
 					<div class="left-top-bar">
-						Free shipping for standard order over $100
+						Bienvenido Stive
 					</div>
 				</li>
 
@@ -175,15 +157,13 @@
 						</a>
 
 						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							My Account
+							Mi Cuenta
 						</a>
 
-						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							EN
-						</a>
+						
 
 						<a href="#" class="flex-c-m p-lr-10 trans-04">
-							USD
+							Salir
 						</a>
 					</div>
 				</li>
@@ -191,35 +171,22 @@
 
 			<ul class="main-menu-m">
 				<li>
-					<a href="index.html">Home</a>
-					<ul class="sub-menu-m">
-						<li><a href="index.html">Homepage 1</a></li>
-						<li><a href="home-02.html">Homepage 2</a></li>
-						<li><a href="home-03.html">Homepage 3</a></li>
-					</ul>
-					<span class="arrow-main-menu-m">
-						<i class="fa fa-angle-right" aria-hidden="true"></i>
-					</span>
+					<a href="<?= base_url(); ?>">Inicio</a>
+					
+					
 				</li>
 
 				<li>
-					<a href="product.html">Shop</a>
+					<a href="<?= base_url(); ?>/tienda">Tienda</a>
+				</li>
+
+
+				<li>
+					<a href="<?= base_url(); ?>/nosotros">Nosotros</a>
 				</li>
 
 				<li>
-					<a href="shoping-cart.html" class="label1 rs1" data-label1="hot">Features</a>
-				</li>
-
-				<li>
-					<a href="blog.html">Blog</a>
-				</li>
-
-				<li>
-					<a href="about.html">About</a>
-				</li>
-
-				<li>
-					<a href="contact.html">Contact</a>
+					<a href="<?= base_url(); ?>/contacto">Contacto</a>
 				</li>
 			</ul>
 		</div>
